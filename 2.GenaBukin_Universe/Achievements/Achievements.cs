aad593bc91389bb43de2e552f8fee4c45dcd9134
Hi-Tech-Mechanic <157356548+Hi-Tech-Mechanic@users.noreply.github.com>
2024-07-09 09:44:44 +0300

    Add files via upload

---
+++ 2.GenaBukin_Universe/Achievements/Achievements.cs
@@ -0,0 +1,13 @@
+namespace GenaBukinUniverse
+{
+    public class Achievements : AchievementsParent
+    {
+        private int[] _neededPurchasedMemeClips = { 1, 5, 10, 15, 19 };
+
+        public override void Init()
+        {
+            NeededPurchasedMemeClips = _neededPurchasedMemeClips;
+            base.Init();
+        }
+    }
+}
